2a3551dae22babc36dace76b0fb93ed888c314dc
calebedwards21 <caleb.r.edwards@gmail.com>
2020-11-10 16:21:04 -0700

    Updated pi server / cleaned it up / made it output correct scraper data

--- pi_server.py
+++ pi_server.py
@@ -5,21 +5,26 @@ from influxdb import InfluxDBClient
 from sprinkler_ml import ML_Sprinkler 
 from weather_scraper import Scraper
 
-TOPICS = ['zone_1', 'zone_2', 'zone_3', 'weather_station']
-db_client = InfluxDBClient(host='localhost', port=8086)
+db_client = InfluxDBClient(host='localhost', port=8086) #Database
 db_client.create_database('sensor_data')
-model = ML_Sprinkler()
+
+model = ML_Sprinkler() #Machine Learning Model
 model.create_model()
-scraper = Scraper()
+
+scraper = Scraper() #Weather Scraper
 scraper.create_scraper()
+
+TOPICS = ['zone_1', 'zone_2', 'zone_3', 'weather_station'] #Topics to subscribe to
+
 
 def write_hub(temp, humidity):
     """
     Rcv data from the hub and store in the database
     """
-    t = time.localtime()
-    time_stamp = time.strftime("%A, %B %d,%Y %H:%M:%S",t)
-    print(time_stamp)
+    
+    #Create DB input from data received from hub topic
+    time = time.localtime()
+    time_stamp = time.strftime("%A, %B %d,%Y %H:%M:%S", time)
     data = [
     {
         "measurement": "hub_temperature",
@@ -36,13 +41,18 @@ def write_hub(temp, humidity):
         }
     }
     ]
+    
+    #Write data to database
     db_client.write_points(data, database='sensor_data')
+    print(f'End of writing hub')
 
 
-def write_zone(zone, moisture, temp, light):
+def write_zone(zone, moisture, temp, light): #Need to edit this if were not using light
     """
     Rcv data from a zone esp and store in database
     """
+    
+    #Create DB input from data from one of the zone topics
     t = time.localtime()
     time_stamp = time.strftime("%A, %B %d,%Y %H:%M:%S",t)
     print(time_stamp)
@@ -57,24 +67,14 @@ def write_zone(zone, moisture, temp, light):
         }
     }
     ]
+    
+    #Write the data to the database
     db_client.write_points(data, database='sensor_data')
-    print("End of writing zone")
-    predict_watertime(zone)
+    print(f'End of writing zone : {zone}')
     
     # Output scraped data to json files
-    scraper.write_separate()
-
-
-def on_connect(client, userdata, flags, rc):
-    """
-    Connect to the broker and subscribe to all topics
-    """
-    if rc==0:
-        print(('Connection OK  with result code {0}'.format(rc)))
-    else:
-        print(('Connection Bad with result code {0}'.format(rc)))
-    for topic in TOPICS:
-        client.subscribe(topic)
+    if zone == "zone_1":
+        scraper.write_file()
 
 
 def on_message(client, userdata, msg):
@@ -88,27 +88,25 @@ def on_message(client, userdata, msg):
         print(('{0}F {1}%'.format(t,h)))
         write_hub(t,h)
     elif msg.topic == 'zone_1' or msg.topic == 'zone_2' or msg.topic == 'zone_3':
-        print("Received Zone topic")
-        print((msg.topic))
+        print(f'Received Zone topic : {msg.topic}')
         m,t,l = [float(x) for x in msg.payload.decode('utf-8').split(',')]
         temp = t * 9/5 + 32
-        print(('Temperature: ',temp))
-        print(('Moisture: ',m))
-        print(('Light: ',l))
+        print(f'Temp : {temp}, Moisture : {m}, Light : {l}')
         write_zone(msg.topic,m,temp,l)
     else:
         print((msg.topic))
-
-
-def predict_watertime(zone):
+        
+        
+def on_connect(client, userdata, flags, rc):
     """
-    Uses the data collected last in the db to predict an output
-    value from the ML model
+    Connect to the broker and subscribe to all topics    
     """
-    print("HERE!!!!!")
-    #temp = db_client.query('SELECT LAST("temperature") FROM zone')
-    #print("The temp is ", temp)
-    print("Output: ", model.predict(350, 80, 100, 25)) # m,t,l,h
+    if rc==0:
+        print(('Connection OK  with result code {0}'.format(rc)))
+    else:
+        print(('Connection Bad with result code {0}'.format(rc)))
+    for topic in TOPICS:
+        client.subscribe(topic)
 
 
 def esp_data():
@@ -119,19 +117,12 @@ def esp_data():
     client.on_connect = on_connect
     time.sleep(2)
     client.on_message = on_message
-
-    connected = 0
-#   while connected == 0:
-#       try:
-#           client.connect('localhost', 1883, 60)
-#           connected = 1   
-#       except:
-#           print("retrying connection")
-#           time.sleep(1)
     client.connect('localhost', 1883, 60)
     client.loop_forever()
 
 
-# t = threading.Thread(target=esp_data)
-# t.start()
-esp_data()
+if __name__ == "__main__":
+    """
+    Starting point for server
+    """
+    esp_data()
