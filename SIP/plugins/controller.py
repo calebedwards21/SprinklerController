90dfef5d56265fe5f57f1fde2ac1d2401caacbc9
WilliRya <u0838551@utah.edu>
2020-11-27 12:53:21 -0700

    Worked with Patricka nd confirmed integration code is working. Will now move onto optimizing adjustment changes

--- SIP/plugins/controller.py
+++ SIP/plugins/controller.py
@@ -28,6 +28,38 @@ from gpiozero import Button
 # Influx DB
 db_client = InfluxDBClient(host='localhost', port=8086)
 
+# Empty settings dictionary to store all the data from the file.
+settings = {}
+# Empty programs list to store the programs of the SIP.
+programs = []
+
+def load_prog():
+    global programs
+    try:
+        with open(u"./data/programData.json", u"r") as f:  # Read the settings from file
+            programs = json.load(f)
+    except IOError:  # If file does not exist create file with defaults.
+        programs = []
+    return programs
+
+# Read in the settings from the file
+
+
+def load_settings():
+    global settings
+    try:
+        with open(u"./data/controller.json", u"r") as f:  # Open file to read settings
+            settings = json.load(f)
+    except IOError:  # If the file does not exist, create a new file
+        settings = {"watertime": "morning"}
+        with open(u"./data/controller.json", u"w") as f:
+            json.dump(settings, f, indent=2, sort_keys=True)
+    return settings
+
+# Call the load programs and settings methods.
+load_prog()
+load_settings()
+
 # FLow Sensor
 # Tell SIP to not use gpio
 gv.use_gpio_pins = False
@@ -58,7 +90,7 @@ except ImportError:
 urls.extend(
     [
         u"/controller", u"plugins.controller.controller_settings",
-        u"/contrlupdate", u"plugins.controller.save_settings"
+        u"/controllerupdate", u"plugins.controller.save_settings"
     ]
 )
 # fmt: on
@@ -67,9 +99,138 @@ urls.extend(
 gv.plugin_menu.append([_(u"Controller Plugin"), u"/controller"])
 
 ################################################################################
+# Progam Decision Helper Function:                                                 #
+################################################################################
+def programCalculation(settings):
+    try:
+        with open(u"./data/programData.json", u"r") as f:
+            programs = json.load(f)
+    except IOError:  # If file does not exist return empty value
+        programs = []  # Default programs.
+
+    # Load database stuff
+    db_client.switch_database('sensor_data')
+    bme_res = db_client.query('select * from weather_station ORDER BY time DESC limit 1')
+    weather_station = list(bme_res.get_points())
+
+    # add weather load of json
+    try:
+        with open(
+            u"./data/weather.json", u"r"
+        ) as f:
+            weather = json.load(f)
+    except Exception as e:
+        print(u"weather.json file error: ", e)
+        weather = {"FAILED"}
+
+    # Testing
+    print(weather_station)
+    print(settings)
+    print(len(programs))
+    print("Programs load:", programs)
+
+    # Check if the automated program exists
+    temp = []
+    for program in programs:
+        name = program.get('name')
+        if not (name == 'seniorproject2020'):
+            temp.append(program)
+    programs = temp
+
+    print("Programs remv:", programs)
+
+    # Calculate time change
+    custom_programs = []
+
+    # Load station count
+    station_count = [int(digit) for digit in bin(gv.sd['show'][0])[2:]]
+    station_count.reverse()
+
+    for n in range(len(station_count), 8):
+        station_count.append(0)
+
+    gpm = {
+        "1": 3.11,
+        "2": 3.3,
+        "3": 0.015,
+        "4": 0.25,
+        "5": 0.5,
+        "6": 1,
+        "7": 2,
+        "8": 0.2,
+        "9": 0.5,
+        "10": 0
+    }
+
+    for i in range(8):
+        if station_count[i]:
+            # Load zone data
+            # Moisture
+            # Watering Time
+            m = db_client.query('Select * from zone_' + str(i+1) + ' ORDER BY time DESC limit 1')
+            moisture = list(m.get_points())
+            print(moisture)
+
+            w_t = db_client.query('select * from zone_' + str(i+1) + '_water_duration ORDER BY time DESC limit 1')
+            water_time = list(w_t.get_points())
+            print(water_time)
+
+            if ("hrate"+str(i+1)) in settings:
+                gpm["10"] = float(settings["hrate"+str(i+1)])
+
+            precipitation_rate = 96.25 * gpm[settings["htype"+str(i+1)]] / float(settings["area"+str(i+1)])
+
+            print("Precipitation Rate "+str(i+1)+":", precipitation_rate)
+
+            time_minutes = round((float(settings["water"+str(i+1)]) / precipitation_rate) * 60)
+            time_sec = time_minutes * 60
+
+            print("Time Min"+str(i+1)+":", time_minutes)
+            print("Time Sec"+str(i+1)+":", time_sec)
+
+            station_mask = [1 << i]
+
+            start_min = 360
+            duration = [time_sec]
+            stop_min = start_min+time_minutes
+
+            zoneName = "zone_" + str(i+1)
+            # write time to DB
+            write_water_duration(zoneName, float(time_sec), float(time_minutes))
+
+            day_mask = 127
+
+            new_program = {
+                "cycle_min": 0,
+                "day_mask": day_mask,
+                "duration_sec": duration,
+                "enabled": 1,
+                "interval_base_day": 0,
+                "name": "seniorproject2020",
+                "start_min": start_min,
+                "station_mask": station_mask,
+                "stop_min": stop_min,
+                "type": "alldays"
+            }
+
+            custom_programs.append(new_program)
+
+    # Add the new programs
+    for prog in custom_programs:
+        programs.append(prog)
+    print(programs)
+    
+    # Save changed SIP programs to file
+    with open(u"./data/programData.json", u"w") as f2:
+        json.dump(programs, f2,  indent=2)
+
+    load_programs()
+    #END HERE
+
+
+################################################################################
 # Program data function loop:                                                   #
 ################################################################################
-
 
 class ProgramDataLoop(Thread):
 
@@ -89,6 +250,7 @@ class ProgramDataLoop(Thread):
             self._sleep_time -= 1
 
     def run(self):
+        global settings
         print('start of timing loop')
         print(gv.now)
         last_min = 0
@@ -96,12 +258,13 @@ class ProgramDataLoop(Thread):
             if int(gv.now // 60) != last_min:
                 temp = int(gv.now // 60)
                 t = time.localtime()
-                #settings = load_settings()
-                #en = settings['enable']
+                en = settings['enable']
                 time_stamp = time.strftime("%H:%M:%S", t)
                 print(temp)
                 print(time_stamp)
-                #print(en)
+                print(en)
+                print('Program Calculation')
+                programCalculation(settings)
                 last_min = int(gv.now // 60)
             self._sleep(60)
 
@@ -186,7 +349,7 @@ class FlowSender(Thread):
                 self._sleep(5)
 
 
-#checker_flow = FlowSender()
+checker_flow = FlowSender()
 
 ################################################################################
 # Pressure Sensor function loop:                                               #
@@ -264,7 +427,7 @@ class PressureSender(Thread):
                 self._sleep(5)
 
 
-#checker_pressure = PressureSender()
+checker_pressure = PressureSender()
 
 ################################################################################
 # Program Data Helper functions:                                               #
@@ -481,40 +644,19 @@ def write_pressure_log(ad2):
 
 
 # Empty settings dictionary to store all the data from the file.
-settings = {}
+#settings = {}
 # Empty programs list to store the programs of the SIP.
-programs = []
+#programs = []
 
 # Read in the SIP programs
 
 
-def load_prog():
-    global programs
-    try:
-        with open(u"./data/programData.json", u"r") as f:  # Read the settings from file
-            programs = json.load(f)
-    except IOError:  # If file does not exist create file with defaults.
-        programs = []
-    return programs
 
-# Read in the settings from the file
-
-
-def load_settings():
-    global settings
-    try:
-        with open(u"./data/controller.json", u"r") as f:  # Open file to read settings
-            settings = json.load(f)
-    except IOError:  # If the file does not exist, create a new file
-        settings = {"watertime": "morning"}
-        with open(u"./data/controller.json", u"w") as f:
-            json.dump(settings, f, indent=2, sort_keys=True)
-    return settings
 
 
 # Call the load programs and settings methods.
-load_prog()
-load_settings()
+#load_prog()
+#load_settings()
 
 # Call the load programs helper method to load the programs into SIP.
 load_programs()
@@ -544,7 +686,7 @@ def station_measure_count(name, **kw):
                 # Reset the flow count
                 rate_cnt = 0
                 # Wait a few seconds, then grab the pressure
-                time.sleep(4)
+                #time.sleep(4)
                 press = get_now_measure()
                 press = get_volt(press)
                 press = get_psi(press)
@@ -630,131 +772,12 @@ class save_settings(ProtectedPage):
         # Dictionary of values returned as query string from settings page.
         settings = (web.input())
         print(settings)
-        try:
-            with open(u"./data/programData.json", u"r") as f:
-                programs = json.load(f)
-        except IOError:  # If file does not exist return empty value
-            programs = []  # Default programs.
-
-        # Load database stuff
-        db_client.switch_database('sensor_data')
-        bme_res = db_client.query('select * from weather_station ORDER BY time DESC limit 1')
-        weather_station = list(bme_res.get_points())
-
-        # add weather load of json
-        try:
-            with open(
-                u"./data/weather.json", u"r"
-            ) as f:
-                weather = json.load(f)
-        except Exception as e:
-            print(u"weather.json file error: ", e)
-            weather = {"FAILED"}
-
-        # Testing
-        print(weather_station)
-        print(settings)
-        print(len(programs))
-        print("Programs load:", programs)
-
-        # Check if the automated program exists
-        temp = []
-        for program in programs:
-            name = program.get('name')
-            if not (name == 'seniorproject2020'):
-                temp.append(program)
-        programs = temp
-
-        print("Programs remv:", programs)
-
-        # Calculate time change
-        custom_programs = []
-
-        # Load station count
-        station_count = [int(digit) for digit in bin(gv.sd['show'][0])[2:]]
-        station_count.reverse()
-
-        for n in range(len(station_count), 8):
-            station_count.append(0)
-
-        gpm = {
-            "1": 3.11,
-            "2": 3.3,
-            "3": 0.015,
-            "4": 0.25,
-            "5": 0.5,
-            "6": 1,
-            "7": 2,
-            "8": 0.2,
-            "9": 0.5,
-            "10": 0
-        }
-
-        for i in range(8):
-            if station_count[i]:
-                # Load zone data
-                # Moisture
-                # Watering Time
-                m = db_client.query('Select * from zone_' + str(i+1) + ' ORDER BY time DESC limit 1')
-                moisture = list(m.get_points())
-                print(moisture)
-
-                w_t = db_client.query('select * from zone_' + str(i+1) + '_water_duration ORDER BY time DESC limit 1')
-                water_time = list(w_t.get_points())
-                print(water_time)
-
-                if ("hrate"+str(i+1)) in settings:
-                    gpm["10"] = float(settings["hrate"+str(i+1)])
-
-                precipitation_rate = 96.25 * gpm[settings["htype"+str(i+1)]] / float(settings["area"+str(i+1)])
-
-                print("Precipitation Rate "+str(i+1)+":", precipitation_rate)
-
-                time_minutes = round((float(settings["water"+str(i+1)]) / precipitation_rate) * 60)
-                time_sec = time_minutes * 60
-
-                print("Time Min"+str(i+1)+":", time_minutes)
-                print("Time Sec"+str(i+1)+":", time_sec)
-
-                station_mask = [1 << i]
-
-                start_min = 360
-                duration = [time_sec]
-                stop_min = start_min+time_minutes
-
-                zoneName = "zone_" + str(i+1)
-                # write time to DB
-                write_water_duration(zoneName, float(time_sec), float(time_minutes))
-
-                day_mask = 127
-
-                new_program = {
-                    "cycle_min": 0,
-                    "day_mask": day_mask,
-                    "duration_sec": duration,
-                    "enabled": 1,
-                    "interval_base_day": 0,
-                    "name": "seniorproject2020",
-                    "start_min": start_min,
-                    "station_mask": station_mask,
-                    "stop_min": stop_min,
-                    "type": "alldays"
-                }
-
-                custom_programs.append(new_program)
-
-        # Add the new programs
-        for prog in custom_programs:
-            programs.append(prog)
-        print(programs)
-
+        
+        programCalculation(settings)
+        
         # Save settings to file
         with open(u"./data/controller.json", u"w") as f1:
             json.dump(settings, f1,  indent=2)  # save to file
-        # Save changed SIP programs to file
-        with open(u"./data/programData.json", u"w") as f2:
-            json.dump(programs, f2,  indent=2)
-
-        load_programs()
+        
 
         raise web.seeother(u"/")  # Return user to home page.
